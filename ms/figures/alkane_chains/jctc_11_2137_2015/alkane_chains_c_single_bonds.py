d0ccc0f821d099c345d36e966186dfded136315f
Janus Juul Eriksen <januseriksen@gmail.com>
2019-06-19 14:47:38 +0100

    update plots

--- ms/figures/alkane_chains/jctc_11_2137_2015/alkane_chains_c_single_bonds.py
+++ ms/figures/alkane_chains/jctc_11_2137_2015/alkane_chains_c_single_bonds.py
@@ -24,46 +24,53 @@ def fit(func, x, y):
 	r_2 = 1. - (ss_res / ss_tot)
 	return opt, r_2
 
+# bond string
+
+bond = 'C-C'
+
 # data
 
-e_c6h14 = np.array([-15.683,-15.339,-14.136])
-e_c7h16 = np.array([-16.591,-16.056,-14.725])
-e_c8h18 = np.array([-17.500,-17.308,-16.645,-15.226])
-e_c9h20 = np.array([-18.216,-17.897,-17.146,-15.661])
-e_c10h22 = np.array([-18.933,-18.806,-18.399,-17.582,-16.047])
-e_c11h24 = np.array([-19.523,-19.307,-18.834,-17.967,-16.392])
-e_c12h26 = np.array([-20.112,-20.024,-19.742,-19.219,-18.312,-16.705])
-e_c13h28 = np.array([-20.613,-20.459,-20.128,-19.565,-18.625,-16.991])
+e_1 = np.array([-15.683,-15.339,-14.136]) # c6h14
+e_2 = np.array([-16.591,-16.056,-14.725]) # c7h16
+e_3 = np.array([-17.500,-17.308,-16.645,-15.226]) # c8h18
+e_4 = np.array([-18.216,-17.897,-17.146,-15.661]) # c9h20
+e_5 = np.array([-18.933,-18.806,-18.399,-17.582,-16.047]) # c10h22
+e_6 = np.array([-19.523,-19.307,-18.834,-17.967,-16.392]) # c11h24
+e_7 = np.array([-20.112,-20.024,-19.742,-19.219,-18.312,-16.705]) # c12h26
+e_8 = np.array([-20.613,-20.459,-20.128,-19.565,-18.625,-16.991]) # c13h28
 
 # bond types
 
-type_alpha = np.array([e_c6h14[-1], e_c7h16[-1], e_c8h18[-1], e_c9h20[-1], e_c10h22[-1], e_c11h24[-1], e_c12h26[-1], e_c13h28[-1]])
+type_alpha = np.array([e_1[-1], e_2[-1], e_3[-1], e_4[-1], e_5[-1], e_6[-1], e_7[-1], e_8[-1]])
 atoms_alpha = np.arange(6,14)
-type_beta = np.array([e_c6h14[-2], e_c7h16[-2], e_c8h18[-2], e_c9h20[-2], e_c10h22[-2], e_c11h24[-2], e_c12h26[-2], e_c13h28[-2]])
-atoms_beta = np.arange(6,14)
-type_gamma = np.array([e_c6h14[-3], e_c7h16[-3], e_c8h18[-3], e_c9h20[-3], e_c10h22[-3], e_c11h24[-3], e_c12h26[-3], e_c13h28[-3]])
-atoms_gamma = np.arange(6,14)
-type_delta = np.array([e_c8h18[-4], e_c9h20[-4], e_c10h22[-4], e_c11h24[-4], e_c12h26[-4], e_c13h28[-4]])
-atoms_delta = np.arange(8,14)
-type_epsilon = np.array([e_c10h22[-5], e_c11h24[-5], e_c12h26[-5], e_c13h28[-5]])
-atoms_epsilon = np.arange(10,14)
-type_zeta = np.array([e_c12h26[-6], e_c13h28[-6]])
-atoms_zeta = np.arange(12,14)
-
-# plotting domains
-
 domain_alpha = np.linspace(atoms_alpha[0], atoms_alpha[-1], \
                            num=(atoms_alpha[-1] - atoms_alpha[0]) * 100)
+type_beta = np.array([e_1[-2], e_2[-2], e_3[-2], e_4[-2], e_5[-2], e_6[-2], e_7[-2], e_8[-2]])
+atoms_beta = np.arange(6,14)
 domain_beta = np.linspace(atoms_beta[0], atoms_beta[-1], \
                            num=(atoms_beta[-1] - atoms_beta[0]) * 100)
+type_gamma = np.array([e_1[-3], e_2[-3], e_3[-3], e_4[-3], e_5[-3], e_6[-3], e_7[-3], e_8[-3]])
+atoms_gamma = np.arange(6,14)
 domain_gamma = np.linspace(atoms_gamma[0], atoms_gamma[-1], \
                            num=(atoms_gamma[-1] - atoms_gamma[0]) * 100)
+type_delta = np.array([e_3[-4], e_4[-4], e_5[-4], e_6[-4], e_7[-4], e_8[-4]])
+atoms_delta = np.arange(8,14)
 domain_delta = np.linspace(atoms_delta[0], atoms_delta[-1], \
                            num=(atoms_delta[-1] - atoms_delta[0]) * 100)
+type_epsilon = np.array([e_5[-5], e_6[-5], e_7[-5], e_8[-5]])
+atoms_epsilon = np.arange(10,14)
 domain_epsilon = np.linspace(atoms_epsilon[0], atoms_epsilon[-1], \
                            num=(atoms_epsilon[-1] - atoms_epsilon[0]) * 100)
+type_zeta = np.array([e_7[-6], e_8[-6]])
+atoms_zeta = np.arange(12,14)
 domain_zeta = np.linspace(atoms_zeta[0], atoms_zeta[-1], \
                            num=(atoms_zeta[-1] - atoms_zeta[0]) * 100)
+
+shift = np.array([e_1[0], e_3[0], e_5[0], e_7[0]])
+atoms_shift = np.arange(6,14,2)
+domain_shift = np.linspace(atoms_shift[0], atoms_shift[-1], \
+                           num=(atoms_shift[-1] - atoms_shift[0]) * 100)
+
 
 sns.set(style='darkgrid', font='DejaVu Sans')
 
@@ -72,31 +79,34 @@ fig, ax = plt.subplots()
 palette = sns.color_palette('Set2')
 
 opt, r_2 = fit(func, atoms_alpha, type_alpha)
-ax.plot(domain_alpha, func(domain_alpha, *opt), linewidth=2, linestyle='-', color=palette[0], label='C-C [$\\alpha$] ($r^2 = {:.3f}$)'.format(r_2))
-print('\n\nf[alpha]   = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_alpha, func(domain_alpha, *opt), linewidth=2, linestyle='-', color=palette[0], label='{:} [$\\alpha$]'.format(bond))
+print('\n\nf[alpha]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_alpha, type_alpha, s=150, marker='.', color=palette[0])
 opt, r_2 = fit(func, atoms_beta, type_beta)
-ax.plot(domain_beta, func(domain_beta, *opt), linewidth=2, linestyle='-', color=palette[2], label='C-C [$\\beta$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[beta]    = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_beta, func(domain_beta, *opt), linewidth=2, linestyle='-', color=palette[2], label='{:} [$\\beta$]'.format(bond))
+print('f[beta]    = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_beta, type_beta, s=150, marker='.', color=palette[2])
 opt, r_2 = fit(func, atoms_gamma, type_gamma)
-ax.plot(domain_gamma, func(domain_gamma, *opt), linewidth=2, linestyle='-', color=palette[3], label='C-C [$\\gamma$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[gamma]   = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_gamma, func(domain_gamma, *opt), linewidth=2, linestyle='-', color=palette[3], label='{:} [$\\gamma$]'.format(bond))
+print('f[gamma]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_gamma, type_gamma, s=150, marker='.', color=palette[3])
 opt, r_2 = fit(func, atoms_delta, type_delta)
-ax.plot(domain_delta, func(domain_delta, *opt), linewidth=2, linestyle='-', color=palette[4], label='C-C [$\\delta$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[delta]   = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_delta, func(domain_delta, *opt), linewidth=2, linestyle='-', color=palette[4], label='{:} [$\\delta$]'.format(bond))
+print('f[delta]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_delta, type_delta, s=150, marker='.', color=palette[4])
 opt, r_2 = fit(func, atoms_epsilon, type_epsilon)
-ax.plot(domain_epsilon, func(domain_epsilon, *opt), linewidth=2, linestyle='-', color=palette[1], label='C-C [$\\epsilon$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[epsilon] = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_epsilon, func(domain_epsilon, *opt), linewidth=2, linestyle='-', color=palette[1], label='{:} [$\\epsilon$]'.format(bond))
+print('f[epsilon] = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_epsilon, type_epsilon, s=150, marker='.', color=palette[1])
 #opt, r_2 = fit(func, atoms_zeta, type_zeta)
-#ax.plot(domain_zeta, func(domain_zeta, *opt), linewidth=2, linestyle='-', color=palette[5], label='C-C [$\\zeta$] ($r^2 = {:.3f}$)'.format(r_2))
-#print('f[zeta]    = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}\n\n'.format(*opt, r_2))
-ax.plot(atoms_zeta, type_zeta, linewidth=2, linestyle='-', color=palette[5], label='C-C [$\\zeta$] ($r^2 = {:.3f}$)'.format(1.))
-print('f[zeta]    = None\n\n')
+#ax.plot(domain_zeta, func(domain_zeta, *opt), linewidth=2, linestyle='-', color=palette[5], label='{:} [$\\zeta$]'.format(bond))
+#print('f[zeta]    = {:.2f} * N {:+2f} / N {:+.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(atoms_zeta, type_zeta, linewidth=2, linestyle='-', color=palette[5], label='{:} [$\\zeta$]'.format(bond))
+print('f[zeta]    = None')
 ax.scatter(atoms_zeta, type_zeta, s=150, marker='.', color=palette[5])
+
+opt, r_2 = fit(func, atoms_shift, shift)
+print('\nf[shift]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}\n\n'.format(*opt, r_2))
 
 ax.xaxis.grid(False)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
--- ms/figures/polyacetylenes/chem_sci_10_1716_2019/polyacetylenes_c_double_bonds.py
+++ ms/figures/polyacetylenes/chem_sci_10_1716_2019/polyacetylenes_c_double_bonds.py
@@ -24,36 +24,43 @@ def fit(func, x, y):
 	r_2 = 1. - (ss_res / ss_tot)
 	return opt, r_2
 
+# bond string
+
+bond = 'C=C'
+
 # data
 
-e_c6h8 = np.array([-27.861,-25.182])
-e_c8h10 = np.array([-30.779,-27.149])
-e_c10h12 = np.array([-33.693,-32.742,-28.629])
-e_c12h14 = np.array([-35.655,-34.220,-29.816])
-e_c14h16 = np.array([-37.616,-37.133,-35.405,-30.806])
-e_c16h18 = np.array([-39.093,-38.318,-36.395,-31.656])
+e_1 = np.array([-27.861,-25.182]) # c6h8
+e_2 = np.array([-30.779,-27.149]) # c8h10
+e_3 = np.array([-33.693,-32.742,-28.629]) # c10h12
+e_4 = np.array([-35.655,-34.220,-29.816]) # c12h14
+e_5 = np.array([-37.616,-37.133,-35.405,-30.806]) # c14h16
+e_6 = np.array([-39.093,-38.318,-36.395,-31.656]) # c16h18
 
 # bond types
 
-type_alpha = np.array([e_c6h8[-1], e_c8h10[-1], e_c10h12[-1], e_c12h14[-1], e_c14h16[-1], e_c16h18[-1]])
+type_alpha = np.array([e_1[-1], e_2[-1], e_3[-1], e_4[-1], e_5[-1], e_6[-1]])
 atoms_alpha = np.arange(6,17,2)
-type_beta = np.array([e_c6h8[-2], e_c8h10[-2], e_c10h12[-2], e_c12h14[-2], e_c14h16[-2], e_c16h18[-2]])
-atoms_beta = np.arange(6,17,2)
-type_gamma = np.array([e_c10h12[-3], e_c12h14[-3], e_c14h16[-3], e_c16h18[-3]])
-atoms_gamma = np.arange(10,17,2)
-type_delta = np.array([e_c14h16[-4], e_c16h18[-4]])
-atoms_delta = np.arange(14,17,2)
-
-# plotting domains
-
 domain_alpha = np.linspace(atoms_alpha[0], atoms_alpha[-1], \
                            num=(atoms_alpha[-1] - atoms_alpha[0]) * 100)
+type_beta = np.array([e_1[-2], e_2[-2], e_3[-2], e_4[-2], e_5[-2], e_6[-2]])
+atoms_beta = np.arange(6,17,2)
 domain_beta = np.linspace(atoms_beta[0], atoms_beta[-1], \
                            num=(atoms_beta[-1] - atoms_beta[0]) * 100)
+type_gamma = np.array([e_3[-3], e_4[-3], e_5[-3], e_6[-3]])
+atoms_gamma = np.arange(10,17,2)
 domain_gamma = np.linspace(atoms_gamma[0], atoms_gamma[-1], \
                            num=(atoms_gamma[-1] - atoms_gamma[0]) * 100)
+type_delta = np.array([e_5[-4], e_6[-4]])
+atoms_delta = np.arange(14,17,2)
 domain_delta = np.linspace(atoms_delta[0], atoms_delta[-1], \
                            num=(atoms_delta[-1] - atoms_delta[0]) * 100)
+
+shift = np.array([e_1[0], e_3[0], e_5[0]])
+atoms_shift = np.arange(6,15,4)
+domain_shift = np.linspace(atoms_shift[0], atoms_shift[-1], \
+                           num=(atoms_shift[-1] - atoms_shift[0]) * 100)
+
 
 sns.set(style='darkgrid', font='DejaVu Sans')
 
@@ -62,29 +69,32 @@ fig, ax = plt.subplots()
 palette = sns.color_palette('Set2')
 
 opt, r_2 = fit(func, atoms_alpha, type_alpha)
-ax.plot(domain_alpha, func(domain_alpha, *opt), linewidth=2, linestyle='-', color=palette[0], label='C=C [$\\alpha$] ($r^2 = {:.3f}$)'.format(r_2))
-print('\n\nf[alpha] = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_alpha, func(domain_alpha, *opt), linewidth=2, linestyle='-', color=palette[0], label='{:} [$\\alpha$]'.format(bond))
+print('\n\nf[alpha]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_alpha, type_alpha, s=150, marker='.', color=palette[0])
 opt, r_2 = fit(func, atoms_beta, type_beta)
-ax.plot(domain_beta, func(domain_beta, *opt), linewidth=2, linestyle='-', color=palette[2], label='C=C [$\\beta$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[beta]  = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_beta, func(domain_beta, *opt), linewidth=2, linestyle='-', color=palette[2], label='{:} [$\\beta$]'.format(bond))
+print('f[beta]    = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_beta, type_beta, s=150, marker='.', color=palette[2])
 opt, r_2 = fit(func, atoms_gamma, type_gamma)
-ax.plot(domain_gamma, func(domain_gamma, *opt), linewidth=2, linestyle='-', color=palette[3], label='C=C [$\\gamma$] ($r^2 = {:.3f}$)'.format(r_2))
-print('f[gamma] = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}'.format(*opt, r_2))
+ax.plot(domain_gamma, func(domain_gamma, *opt), linewidth=2, linestyle='-', color=palette[3], label='{:} [$\\gamma$]'.format(bond))
+print('f[gamma]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
 ax.scatter(atoms_gamma, type_gamma, s=150, marker='.', color=palette[3])
 #opt, r_2 = fit(func, atoms_delta, type_delta)
-#ax.plot(domain_delta, func(domain_delta, *opt), linewidth=2, linestyle='-', color=palette[1], label='C=C [$\\delta$] ($r^2 = {:.3f}$)'.format(r_2))
-#print('f[delta] = {:.2f} * N {:+2f} / N {:.2f} --- r_2 = {:.3f}\n\n'.format(*opt, r_2))
-ax.plot(atoms_delta, type_delta, linewidth=2, linestyle='-', color=palette[4], label='C=C [$\\delta$]  ($r^2 = {:.3f}$)'.format(1.))
-print('f[delta] = None\n\n')
+#ax.plot(domain_delta, func(domain_delta, *opt), linewidth=2, linestyle='-', color=palette[4], label='{:} [$\\delta$]'.format(bond))
+#print('f[delta]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}'.format(*opt, r_2))
+ax.plot(atoms_delta, type_delta, linewidth=2, linestyle='-', color=palette[4], label='{:} [$\\delta$]'.format(bond))
+print('f[delta]   = None')
 ax.scatter(atoms_delta, type_delta, s=150, marker='.', color=palette[4])
+
+opt, r_2 = fit(func, atoms_shift, shift)
+print('\nf[shift]   = {:.2f} * N  {:+.2f} / N  {:+.2f} --- r_2 = {:.5f}\n\n'.format(*opt, r_2))
 
 ax.xaxis.grid(False)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 ax.set_xlabel('Number of Carbon Atoms')
 ax.set_ylabel('Energy Contribution (in au)')
-ax.legend(loc='lower left', markerscale=0, frameon=False)
+ax.legend(loc='lower left', frameon=False)
 
 sns.despine()
 plt.savefig('polyacetylenes_c_double_bonds.pdf', bbox_inches = 'tight', dpi=1000)
