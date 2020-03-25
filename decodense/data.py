350e9c702e81b797abf67bb92fd7fc220fb8617a
Janus Juul Eriksen <januseriksen@gmail.com>
2020-03-25 09:08:27 +0000

    data.py

---
+++ decodense/data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""
+data module
+"""
+
+__author__ = 'Dr. Janus Juul Eriksen, University of Bristol, UK'
+__maintainer__ = 'Dr. Janus Juul Eriksen'
+__email__ = 'janus.eriksen@bristol.ac.uk'
+__status__ = 'Development'
+
+# https://en.wikipedia.org/wiki/Hartree
+AU_TO_KCAL_MOL = 627.5094740631
+AU_TO_EV = 27.211386245988
+AU_TO_KJ_MOL = 2625.4996394799
+# https://calculla.com/dipole_moment_units_converter
+AU_TO_DEBYE = 2.54174623
+
