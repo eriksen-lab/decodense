a8c19c599c21063d9ae15914c09d2614dc30c3c0
Janus Juul Eriksen <januseriksen@gmail.com>
2022-01-10 14:54:16 +0100

    clean up

--- decodense/decodense.py
+++ decodense/decodense.py
@@ -41,23 +41,27 @@ def main(mol: gto.Mole, decomp: DecompCls, \
         if decomp.loc != '':
             mo_coeff = loc_orbs(mol, mo_coeff, mo_occ, decomp.loc, loc_lst)
 
-        # inter-atomic distance array
-        dist = gto.mole.inter_distance(mol) * lib.param.BOHR
-
         # decompose property
         if decomp.part in ['atoms', 'eda']:
+            # compute population weights
             weights = assign_rdm1s(mol, mo_coeff, mo_occ, decomp.pop, decomp.part, \
                                    multiproc = decomp.multiproc, verbose = decomp.verbose)[0]
+            # compute decomposed results
             decomp.res = prop_tot(mol, mf, mo_coeff, mo_occ, decomp.pop, \
                                   decomp.prop, decomp.part, decomp.multiproc, \
                                   weights = weights, dipole_origin = dipole_origin)
         elif decomp.part == 'bonds':
+            # compute repetitive indices & centres
             rep_idx, centres = assign_rdm1s(mol, mo_coeff, mo_occ, decomp.pop, decomp.part, \
                                             multiproc = decomp.multiproc, verbose = decomp.verbose, \
                                             thres = decomp.thres)
+            # compute decomposed results
             decomp.res = prop_tot(mol, mf, mo_coeff, mo_occ, decomp.pop, \
                                   decomp.prop, decomp.part, decomp.multiproc, \
                                   rep_idx = rep_idx, dipole_origin = dipole_origin)
+            # save centres & inter-atomic distances
+            decomp.res['centres'] = centres
+            decomp.res['dist'] = gto.mole.inter_distance(mol) * lib.param.BOHR
 
         # write rdm1s
         if decomp.write != '':
@@ -67,11 +71,6 @@ def main(mol: gto.Mole, decomp: DecompCls, \
 
         # collect time
         decomp.res['time'] = MPI.Wtime() - time
-
-        # collect centres & dist
-        if decomp.part == 'bonds':
-            decomp.res['centres'] = centres
-            decomp.res['dist'] = dist
 
         return decomp.res
 
