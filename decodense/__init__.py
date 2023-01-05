7682a378dd91ea01d4f35da38f25e5e5ca5f3620
Janus Juul Eriksen <januseriksen@gmail.com>
2023-01-05 14:51:42 +0100

    remove data.py from __init__.py

--- decodense/__init__.py
+++ decodense/__init__.py
@@ -3,4 +3,3 @@ from .decomp import CompKeys, DecompCls
 from .orbitals import assign_rdm1s
 from .tools import mf_info, make_natorb, write_rdm1, res_add, res_sub
 from .results import info
-from .data import *
