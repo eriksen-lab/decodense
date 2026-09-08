dc43b27329b7827968a4e37057f801f6bb28e53d
Magda <mlubu@dtu.dk>
2026-09-08 15:13:11 +0200

    Remove unnecessary fmt export from __init__.py

--- decodense/__init__.py
+++ decodense/__init__.py
@@ -2,4 +2,4 @@ from .decodense import main
 from .decomp import CompKeys, DecompCls
 from .orbitals import assign_rdm1s
 from .tools import mf_info, make_natorb, write_rdm1, res_add, res_sub
-from .results import info, fmt
+from .results import info
